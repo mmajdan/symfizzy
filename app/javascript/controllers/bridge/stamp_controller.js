2d1e2019460a6a3c52f995ea0d9c38dc52dec9b4
Adrien Maston <adrien@37signals.com>
2026-03-17 11:57:59 +0100

    Add scope selector value

--- app/javascript/controllers/bridge/stamp_controller.js
+++ app/javascript/controllers/bridge/stamp_controller.js
@@ -2,19 +2,18 @@ import { BridgeComponent } from "@hotwired/hotwire-native-bridge"
 
 export default class extends BridgeComponent {
   static component = "stamp"
+  static values = { scopeSelector: { type: String, default: "body" } }
 
   connect() {
-    super.connect()
-    this.notifyBridgeOfConnect()
+    if (this.element.closest(this.scopeSelectorValue)) {
+      super.connect()
+      this.send("connect", this.#data)
+    }
   }
 
   disconnect() {
     super.disconnect()
     this.send("disconnect")
-  }
-
-  notifyBridgeOfConnect() {
-    this.send("connect", this.#data)
   }
 
   get #data() {
